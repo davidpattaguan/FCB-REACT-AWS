0abfdc66c366c20e8c854ce48521ff47d750acb7
David Niño Angelo Pattaguan <wpattaguandna@gmail.com>
2022-01-20 16:44:51 +0800

    Create a new tasklist component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const TasksList = () => {
+  return (
+    <ul>
+      <li className="tasks-item">Follow Edukasyon.ph on Facebook</li>
+      <li className="tasks-item">Follow AWS Siklab Pilipinas on Facebook</li>
+      <li className="tasks-item">Follow Zuittt Coding Bootcamp on Facebook</li>
+    </ul>
+  );
+};
+
 const App = () => {
   const paraStyle = {
     marginBottom: "20px",
@@ -14,15 +24,7 @@ const App = () => {
         <p style={paraStyle}>
           The most simple and amazing todo-list React app.
         </p>
-        <ul>
-          <li className="tasks-item">Follow Edukasyon.ph on Facebook</li>
-          <li className="tasks-item">
-            Follow AWS Siklab Pilipinas on Facebook
-          </li>
-          <li className="tasks-item">
-            Follow Zuittt Coding Bootcamp on Facebook
-          </li>
-        </ul>
+        <TasksList />
       </header>
     </div>
   );
